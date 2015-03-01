08dc7f2ea82fdf27b3bdf4335b9b694603344739
Philip Klostermann <philip.klostermann@gmail.com>
2015-03-01 03:25:49 -0500

    split comment stream off into separate project

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var db = require("./database.js");
 var videoID = process.argv[2];
-
 if(!videoID) {
 	console.error("No Video ID!");
 	process.exit(1);
 }
 
+
+var db = require("./lib/database.js");
 /* Create a new table, overwrite if exists */
 db.createTable(videoID, true);
 
-var scraper = require("./lib/comment-scraper.js")(videoID);
+var scraper = require("youtube-comment-stream")(videoID);
+var totalComments = 0;
 
+console.log("Scraping comments");
 scraper.on('data', function(comment) {
 	db.addComments([comment], videoID);
+	
+	if(++totalComments % 100 == 0) {
+		console.log("Scraped " + totalComments + " comments so far");
+	}
 });
+
+scraper.on('end', function() {
+	console.log("\nScraped " + totalComments + " comments total.");
+	console.log("Committing remaining comments to Database...");
+})
