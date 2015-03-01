1f79df42f7e5ce51ce974e6857924cb65d894caf
Philip Klostermann <philip.klostermann@gmail.com>
2015-03-01 00:30:41 -0500

    Implemented pager package

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var CommentScraper = require("./lib/comment-scraper.js");
+var scrapeComments = require("./lib/comment-scraper.js");
 var db = require("./database.js");
 var videoID = process.argv[2];
 
@@ -13,47 +13,7 @@ var allComments = [];
 /* Create a new table, overwrite if exists */
 db.createTable(videoID, true);
 
-var commentScraper = new CommentScraper(videoID);
-
-var self = this;
-this.prevComments = [];
-
-var cb = function(error, commentsArr, nextPageToken) {
-	if(error)  {
-		console.error(error);
-		process.exit(1);
-	}
-	
-	deleteOverlap(self.prevComments, commentsArr);
-	db.addComments(commentsArr, videoID);
-	self.prevComments = commentsArr;
-
-	totalComments += commentsArr.length;
-	
-	if(nextPageToken) {
-		console.log("\nComments so far: " + totalComments + "\n");
-		commentScraper.getCommentsPage(nextPageToken, cb);
-	} else {
-		console.log("\nScraped " + totalComments + " comments.");
-	}
-};
-
-commentScraper.getCommentsPage(null, cb);
-
-
-/* Sometimes the last comment on one page is the same as the first comment on the
- * next page. It's definitely Youtube's fault!
- * This function gets rid of the extra comments on the second page (currComments) */
-function deleteOverlap(prevComments, currComments) {
-	for(var i = prevComments.length-1; i >= 0; i--) {
-		if(commentsEqual(prevComments[i], currComments[0]))
-			currComments.splice(0, 1);
-		else
-			break;	
-	}
-}
-
-/* What makes two comments equal? */
-function commentsEqual(c1, c2) {
-	return c1.youtubeCommentID === c2.youtubeCommentID;
-}
+scrapeComments(videoID, function(error, comments) {
+	console.log("\nAdding comments to DB.");
+	db.addComments(comments, videoID);
+});
